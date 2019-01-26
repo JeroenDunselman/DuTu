507fd41d7a1d7181ba749d507eb333d2e54414bc
Jeroen Dunselman <jeroduns@gmail.com>
2019-01-26 17:13:17 +0100

    text add

--- DuTu/ViewItemViewController.swift
+++ DuTu/ViewItemViewController.swift
@@ -45,6 +45,9 @@ class ViewItemViewController: UIViewController, UITextViewDelegate {
         
     }
     
+    
+    //text add
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
